4bb5c231540fc6f333a027e9519087f59b184b68
YingLiWang <YingLiWang@CFHAZC.onmicrosoft.com>
2026-05-28 17:29:17 +0800

    Revert ""
    This reverts commit 33dfa77cc4be431aad36485e5fd51e34b8db0a83.

--- src/router/router.js
+++ src/router/router.js
@@ -6,18 +6,6 @@ export const routes = [
         name: 'home',
         component: BaseLayout,
     },
-    {
-        path: '/A3',
-        name: '/A3',
-        component: BaseLayout,
-        children: [
-            {
-                path: 'DSA30900',
-                name: 'DSA30900',
-                component: () => import('@/views/A3/DSA30900.vue')
-            }
-        ]
-    },
     // 其他業務路由請依照以下範例格式新增路由，並替換路徑、名稱與元件引用路徑
     // {
     //     path: {Module},
