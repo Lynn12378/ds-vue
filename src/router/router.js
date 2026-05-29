504ff2057829cc4a055f4f310cad7713a17e4b90
YingLiWang <YingLiWang@CFHAZC.onmicrosoft.com>
2026-05-29 20:59:27 +0800

    /jsp-renovator skipCM=true

--- src/router/router.js
+++ src/router/router.js
@@ -6,6 +6,11 @@ export const routes = [
         name: 'home',
         component: BaseLayout,
     },
+    {
+        path: '/A3/DSA30900',
+        name: 'DSA30900',
+        component: () => import('@/views/A3/DSA30900.vue'),
+    },
     // 其他業務路由請依照以下範例格式新增路由，並替換路徑、名稱與元件引用路徑
     // {
     //     path: {Module},
